2bf9b34b397d48172c3bb495f12bee438293e432
tylerjbrown22 <tylerj.brown@protonmail.ch>
2017-03-16 13:12:12 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@
 		transitionDuration: 2500,
 		delay: 5000,
     	slides: [
-       	{ src: "https://api.mapbox.com/styles/v1/mapbox/satellite-v8/static/-123.8,49.3159,9,0/1280x1014@2x?access_token=pk.eyJ1IjoidGJyb3duIiwiYSI6IlZvempyR3MifQ.UV3klqJHmrE9U6BRB7dENwg" },
+       	{ src: "images/slides/03.jpg" },
         { src: "https://api.mapbox.com/styles/v1/mapbox/dark-v8/static/-123.9,49.1659,11.5,0/1280x1014@2x?access_token=pk.eyJ1IjoidGJyb3duIiwiYSI6IlZvempyR3MifQ.UV3klqJHmrE9U6BRB7dENw" },
         { src: "https://api.mapbox.com/styles/v1/mapbox/dark-v8/static/-123.95,49.159,15,0/1280x1014@2x?access_token=pk.eyJ1IjoidGJyb3duIiwiYSI6IlZvempyR3MifQ.UV3klqJHmrE9U6BRB7dENw" }
     	]
